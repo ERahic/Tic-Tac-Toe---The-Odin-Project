92544b2e54a14540736b9426f24d5a6f4929e424
Edis Rahic <rahic.edis@gmail.com>
2025-07-15 13:41:26 -0400

    Added an IIFE for DOM references to follow the DRY protocol. Added a function to create players and assign them their marks (X, O).

--- script.js
+++ script.js
@@ -1,38 +1,76 @@
+// DOM REFERENCE TO FOLLOW DRY PROTOCOL
+const domReference = (function () {
+  gridContainer = document.querySelector("#gameBoard-container");
+  player1Input = document.querySelector("#player1");
+  player2Input = document.querySelector("#player2");
+  startButton = document.querySelector("#startBtn");
+  resetButton = document.querySelector("#resetBtn");
+
+  return {
+    gridContainer,
+    player1Input,
+    player2Input,
+    startButton,
+    resetButton,
+  };
+})();
+
 // Generate Gameboard
 const gameBoard = (function () {
   const gameBoardArray = ["", "", "", "", "", "", "", "", ""];
 
   //Render the grid
   const render = function () {
-    const gridContainer = document.querySelector("#gameBoard-container");
     gameBoardArray.forEach((square, index) => {
       const squareDiv = document.createElement("div");
       squareDiv.classList.add("square");
       squareDiv.id = `square-${index}`;
-      gridContainer.appendChild(squareDiv);
+      domReference.gridContainer.appendChild(squareDiv);
     });
   };
   return { render };
 })();
 
+// Create the names for each player and assign them their marks
+const playerModule = (function () {
+  let player1;
+  let player2;
+
+  const createPlayers = function (name, mark) {
+    return { name, mark };
+  };
+
+  const initializePlayers = () => {
+    player1 = createPlayers(domReference.player1Input.value.trim(), "X");
+    player2 = createPlayers(domReference.player2Input.value.trim(), "O");
+  };
+
+  const receivePlayers = () => ({ player1, player2 });
+
+  return { initializePlayers, receivePlayers };
+})();
+
 // Generate grid when clicking on start game
 const beginGame = (function () {
-  const startButton = document.querySelector("#startBtn");
-  startButton.addEventListener("click", () => {
+  domReference.startButton.addEventListener("click", () => {
     console.log("Start Button Clicked");
-    const player1 = document.querySelector("#player1").value.trim();
-    const player2 = document.querySelector("#player2").value.trim();
-    if (player1 === "" || player2 === "") {
+    playerModule.initializePlayers();
+    const { player1, player2 } = playerModule.receivePlayers();
+    if (player1.name === "" || player2.name === "") {
       alert("ERROR! Please Enter The Name For Both Players");
       console.log("ERROR! Please Enter The Name For Both Players");
     } else {
-      alert("Begin The Game!");
-      console.log("Game Begin");
-      console.log(`${player1} vs ${player2}`);
+      alert(
+        `Begin The Game! ${player1.name} (${player1.mark}) vs ${player2.name} (${player2.mark})`
+      );
+      console.log("Game Has Begun!");
+      console.log(
+        `${player1.name} (${player1.mark}) vs ${player2.name} (${player2.mark})`
+      );
       gameBoard.render();
       startButton.style.display = "none";
-      document.querySelector("#player1").disabled = true;
-      document.querySelector("#player2").disabled = true;
+      domReference.player1Input.disabled = true;
+      domReference.player2Input.disabled = true;
     }
   });
   return { startButton };
@@ -40,14 +78,13 @@ const beginGame = (function () {
 
 // Reset the Game function
 const resetGame = (function () {
-  const resetButton = document.querySelector("#resetBtn");
-  resetButton.addEventListener("click", function () {
+  domReference.resetButton.addEventListener("click", function () {
     console.log("Reset Button Clicked");
-    document.querySelector("#player1").disabled = false;
-    document.querySelector("#player2").disabled = false;
-    player1.value = "";
-    player2.value = "";
-    document.querySelector("#gameBoard-container").innerHTML = "";
+    domReference.player1Input.disabled = false;
+    domReference.player2Input.disabled = false;
+    domReference.player1Input.value = "";
+    domReference.player2Input.value = "";
+    domReference.gridContainer.innerHTML = "";
     beginGame.startButton.style.display = "flex";
   });
 })();
